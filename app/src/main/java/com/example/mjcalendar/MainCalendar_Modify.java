7459566a38d89b8a42ecedac5aeb5ef59e70dee0
Jiyeon <mycasio1@naver.com>
2020-03-08 02:22:05 +0900

    Update Calendar design

---
+++ app/src/main/java/com/example/mjcalendar/MainCalendar_Modify.java
@@ -0,0 +1,14 @@
+package com.example.mjcalendar;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainCalendar_Modify extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar_modify);
+    }
+}
