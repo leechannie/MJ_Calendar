b366d817b7108af497510d0634f6fe3eca2d083c
leechannie <ioio000213@gmail.com>
2020-03-19 20:45:50 +0900

    파이어베이스 연결 (테스트 전)

--- app/src/main/java/com/example/mjcalendar/Login_Register.java
+++ app/src/main/java/com/example/mjcalendar/Login_Register.java
@@ -1,19 +1,33 @@
 package com.example.mjcalendar;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Login_Register extends AppCompatActivity {
+    private static final String TAG = "SingUp";
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
 
         EditText register_name = (EditText) findViewById(R.id.register_name);
         EditText register_email = (EditText) findViewById(R.id.register_email);
@@ -23,5 +37,48 @@ public class Login_Register extends AppCompatActivity {
         ImageView register_ima = (ImageView) findViewById(R.id.register_ima);
 
         Button register = (Button) findViewById(R.id.register);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              switch (v.getId()){
+                  case R.id.register:
+                      signUp();
+                      break;
+              }
+            }
+        });
+    }@Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    private void signUp(){
+        String email = ((EditText) findViewById(R.id.register_email)).getText().toString();
+        String password = ((EditText) findViewById(R.id.register_pass)).getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //성공ui
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+//                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+//                                    Toast.LENGTH_SHORT).show();
+                            //실패ui
+                        }
+
+                        // ...
+                    }
+                });
+
     }
 }
