d4acb14856fafca3a27e49ca8eb4718ee8aba55d
djyou128 <djyou128@naver.com>
2020-04-08 15:20:43 +0900

    add file about setting for manager

---
+++ app/src/main/java/com/example/mjcalendar/Manager_setting.java
@@ -0,0 +1,22 @@
+package com.example.mjcalendar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.Toolbar;
+
+
+/*
+ Manager User의 Setting option을 접근하기 위한 page
+ */
+public class Manager_setting extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_manager_setting );
+
+
+
+    }
+}
