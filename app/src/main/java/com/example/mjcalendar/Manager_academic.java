8db3f492ffe7db95940a523eaae7634fad1d9eb4
djyou128 <djyou128@naver.com>
2020-04-22 16:11:35 +0900

    add web crawling code

--- app/src/main/java/com/example/mjcalendar/Manager_academic.java
+++ app/src/main/java/com/example/mjcalendar/Manager_academic.java
@@ -1,16 +1,25 @@
 package com.example.mjcalendar;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import android.widget.Toast;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 
 import static com.example.mjcalendar.R.layout.activity_manager_academic;
 
@@ -24,6 +33,9 @@ public class Manager_academic extends AppCompatActivity {
     Button btn_load_update;
     Button btn_mju_home;
     Toolbar toolbar;
+
+    private String htmlPageUrl = "www.mju.ac.kr/mjukr/262/subview.do";
+
 
 
     @Override
@@ -55,5 +67,23 @@ public class Manager_academic extends AppCompatActivity {
                 }
             }
         });
+
+        // 스크롤 가능한 텍스트 뷰로 설정
+        tv_update_log.setMovementMethod( new ScrollingMovementMethod() );
+
+        btn_update_now.setOnClickListener( new Button.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                System.out.println("크롤링 중");
+                try {
+                    Document doc = Jsoup.connect( htmlPageUrl ).get();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
     }
+
+    private class
 }
